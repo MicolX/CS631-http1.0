5b505e5c1063fc66a15fefa84a46141d833ca590
Michael <michael827xmy@hotmail.com>
2021-01-14 22:56:08 -0500

    format tabs, check port number, simplify getopt

--- response.c
+++ response.c
@@ -11,346 +11,401 @@
 
 #include "response.h"
 
-#define MESSAGESIZE     256
-#define OK              0
-#define CREATED         1
-#define ACCEPT          2
-#define NO_CONTENT      3
-#define MOVED_PERM      4
-#define MOVED_TEMP      5
-#define NOT_MODIFIED    6
-#define BAD_REQUEST     7
-#define UNAUTHORIZED    8
-#define FORBIDDEN       9
-#define NOT_FOUND       10
-#define INTERNAL_ERR    11
-#define NOT_IMP         12
-#define BAD_GATEWAY     13
-#define UNAVAILABLE     14
+#define MESSAGESIZE 256
+#define OK 0
+#define CREATED 1
+#define ACCEPT 2
+#define NO_CONTENT 3
+#define MOVED_PERM 4
+#define MOVED_TEMP 5
+#define NOT_MODIFIED 6
+#define BAD_REQUEST 7
+#define UNAUTHORIZED 8
+#define FORBIDDEN 9
+#define NOT_FOUND 10
+#define INTERNAL_ERR 11
+#define NOT_IMP 12
+#define BAD_GATEWAY 13
+#define UNAVAILABLE 14
 
-const char* status[] = {
-        "200 OK\r\n",						
-        "201 Created\r\n",					
-        "202 Accepted\r\n",					
-        "204 No Content\r\n",				        
-        "301 Moved Permanently\r\n",		                
-        "302 Moved Temporarily\r\n",		                
-        "304 Not Modified\r\n",				        
-        "400 Bad Request\r\n",				        
-        "401 Unauthorized\r\n",				        
-        "403 Forbidden\r\n",				      
-        "404 Not Found\r\n",				        
-        "500 Internal Server Error\r\n",	               
-        "501 Not Implemented\r\n",			        
-        "502 Bad Gateway\r\n",				        
-        "503 Service Unavailable\r\n"		               
-};
-
+const char *status[] = {
+	"200 OK\r\n",
+	"201 Created\r\n",
+	"202 Accepted\r\n",
+	"204 No Content\r\n",
+	"301 Moved Permanently\r\n",
+	"302 Moved Temporarily\r\n",
+	"304 Not Modified\r\n",
+	"400 Bad Request\r\n",
+	"401 Unauthorized\r\n",
+	"403 Forbidden\r\n",
+	"404 Not Found\r\n",
+	"500 Internal Server Error\r\n",
+	"501 Not Implemented\r\n",
+	"502 Bad Gateway\r\n",
+	"503 Service Unavailable\r\n"};
 
 /*
  * Uses request data to build, if applicable, the appropriate response.
  */
-int
-respond(char *rootpath, Request *req, Response *res) {
-        if (req->errcode != 0) {
-                switch (req->errcode) {
-                        case 400:
-                                res->status = status[BAD_REQUEST];
-                                break;
-                        case 501:
-                                res->status = status[NOT_IMP];
-                                break;
-                }
-                res->headonly = 1;
-                return 0;
-        }
+int 
+respond(char *rootpath, Request *req, Response *res)
+{
+	if (req->errcode != 0)
+	{
+		switch (req->errcode)
+		{
+		case 400:
+			res->status = status[BAD_REQUEST];
+			break;
+		case 501:
+			res->status = status[NOT_IMP];
+			break;
+		}
+		res->headonly = 1;
+		return 0;
+	}
 
-        struct stat sb;
-        struct tm ifmtime;
-        int fd;
-        magic_t cookie;
-        char uri[MAXPATHLEN];
+	struct stat sb;
+	struct tm ifmtime;
+	int fd;
+	magic_t cookie;
+	char uri[MAXPATHLEN];
 
-		if (req->hastilde == 1) {
-			(void)strncpy(uri, req->uri, MAXPATHLEN);
-		} else {
-        	snprintf(uri, MAXPATHLEN, "%s/%s", rootpath, req->uri);
+	if (req->hastilde == 1)
+	{
+		(void)strncpy(uri, req->uri, MAXPATHLEN);
+	}
+	else
+	{
+		snprintf(uri, MAXPATHLEN, "%s/%s", rootpath, req->uri);
+	}
+
+	if (stat(uri, &sb) == -1)
+	{
+		switch (errno)
+		{
+		case EACCES:
+			syslog(LOG_INFO, "Error: EACCES stat on URI");
+			res->status = status[FORBIDDEN];
+			break;
+		default:
+			syslog(LOG_INFO, "Error performing stat on URI");
+			res->status = status[NOT_FOUND];
+			break;
+		}
+		res->headonly = 1;
+		return 0;
+	}
+
+	res->status = status[OK];
+
+	if (S_ISDIR(sb.st_mode))
+	{
+		char htmlpath[MAXPATHLEN];
+
+		snprintf(htmlpath, MAXPATHLEN, "%s/index.html", uri);
+
+		if (access(htmlpath, F_OK) != 0)
+		{
+			res->dirindex = 1;
+			strlcpy(req->uri, uri, MAXPATHLEN);
+			return 0;
+		}
+		else
+		{
+			res->dirindex = 0;
+			strlcpy(req->uri, htmlpath, MAXPATHLEN);
+		}
+	}
+	else
+	{
+		res->dirindex = 0;
+		strlcpy(req->uri, uri, MAXPATHLEN);
+	}
+
+	if (stat(req->uri, &sb) == -1)
+	{
+		syslog(LOG_INFO, "Error: stat on URI");
+		return -1;
+	}
+
+	res->lastmtime = sb.st_mtime;
+
+	if (strlen(req->ifms) > 0)
+	{
+		if (strptime(req->ifms, "%a, %d %b %Y %T GMT", &ifmtime) == NULL)
+		{
+			if (strptime(req->ifms, "%a, %d-%b-%Y %T GMT", &ifmtime) == NULL)
+			{
+				if (strptime(req->ifms, "%a %b  %d %T %Y", &ifmtime) == NULL)
+				{
+					res->status = status[BAD_REQUEST];
+					res->headonly = 1;
+					return 0;
+				}
+			}
 		}
 
-        if (stat(uri, &sb) == -1) {
-                switch (errno) {
-                        case EACCES:
-                                syslog(LOG_INFO, "Error: EACCES stat on URI");
-                                res->status = status[FORBIDDEN];
-                                break;
-                        default:
-                                syslog(LOG_INFO, "Error performing stat on URI");
-                                res->status = status[NOT_FOUND];
-                                break;
-                }
-                res->headonly = 1;
-                return 0;
-        }
+		if (difftime(sb.st_mtime, mktime(&ifmtime)) > 0)
+		{
+			res->headonly = 0;
+		}
+		else
+		{
+			res->status = status[NOT_MODIFIED];
+			res->headonly = 1;
+			return 0;
+		}
+	}
 
-		res->status = status[OK];
+	if ((fd = open(req->uri, O_RDONLY)) < 0)
+	{
+		syslog(LOG_INFO, "Error opening URI");
+		return -1;
+	}
 
-        if (S_ISDIR(sb.st_mode)) {
-                char htmlpath[MAXPATHLEN];
+	res->contentlength = (long long)sb.st_size;
 
-                snprintf(htmlpath, MAXPATHLEN, "%s/index.html", uri);
+	if ((cookie = magic_open(MAGIC_MIME)) == NULL)
+	{
+		syslog(LOG_INFO, "magic_open error");
+		(void)close(fd);
+		return -1;
+	}
 
-                if (access(htmlpath, F_OK) != 0) {
-                        res->dirindex = 1;
-                        strlcpy(req->uri, uri, MAXPATHLEN);
-                        return 0;
-                } else {
-                        res->dirindex = 0;
-                        strlcpy(req->uri, htmlpath, MAXPATHLEN);
-                }
-        } else {
-                res->dirindex = 0;
-                strlcpy(req->uri, uri, MAXPATHLEN);
-        }
+	if (magic_load(cookie, NULL) != 0)
+	{
+		syslog(LOG_INFO, "magic_load error");
+		(void)magic_close(cookie);
+		(void)close(fd);
+		return -1;
+	}
 
-        if (stat(req->uri, &sb) == -1) {
-                syslog(LOG_INFO, "Error: stat on URI");
-                return -1;
-        }
-        
-        res->lastmtime = sb.st_mtime;
-
-        if (strlen(req->ifms) > 0) {
-                if (strptime(req->ifms, "%a, %d %b %Y %T GMT", &ifmtime) == NULL) {
-                        if (strptime(req->ifms, "%a, %d-%b-%Y %T GMT", &ifmtime) == NULL) {
-                                if (strptime(req->ifms, "%a %b  %d %T %Y", &ifmtime) == NULL) {
-                                        res->status = status[BAD_REQUEST];
-                                        res->headonly = 1;
-                                        return 0;
-                                }
-                        }
-                }
-
-                if (difftime(sb.st_mtime, mktime(&ifmtime)) > 0) {
-                        res->headonly = 0;
-                } else {
-                        res->status = status[NOT_MODIFIED];
-                        res->headonly = 1;
-                        return 0;
-                }
-        }
-
-
-        if ((fd = open(req->uri, O_RDONLY)) < 0) {
-                syslog(LOG_INFO, "Error opening URI");
-                return -1;
-        }
-
-        res->contentlength = (long long)sb.st_size;
-
-        if ((cookie = magic_open(MAGIC_MIME)) == NULL) {
-                syslog(LOG_INFO, "magic_open error");
-                (void)close(fd);
-                return -1;
-        }
-
-        if (magic_load(cookie, NULL) != 0) {
-                syslog(LOG_INFO, "magic_load error");
-                (void)magic_close(cookie);
-                (void)close(fd);
-                return -1;
-        }
-
-        res->contenttype = magic_descriptor(cookie, fd);
-        res->status = status[OK];
-        if (req->method == HEAD) {
-                res->headonly = 1;
-        }
-        return 0;
+	res->contenttype = magic_descriptor(cookie, fd);
+	res->status = status[OK];
+	if (req->method == HEAD)
+	{
+		res->headonly = 1;
+	}
+	return 0;
 }
-
 
 int
 (compar)(const FTSENT **a, const FTSENT **b)
 {
-        return strcmp((*a)->fts_name, (*b)->fts_name);
+	return strcmp((*a)->fts_name, (*b)->fts_name);
 }
 
 /*
  * Sends the response (including file transversal work, if applicable) back the client.
  */
-int
-reply(int socket, Request *req, Response *res) {
-        char message[MESSAGESIZE];
-        char buf[BUFSIZ];
-        int fd;
-        ssize_t readsize;
-        ssize_t writesize;
+int 
+reply(int socket, Request *req, Response *res)
+{
+	char message[MESSAGESIZE];
+	char buf[BUFSIZ];
+	int fd;
+	ssize_t readsize;
+	ssize_t writesize;
 
-        if (req->errcode != 0) {
-                snprintf(message, sizeof(message), "HTTP/1.0 %s\r\n", res->status);
-                if (write(socket, message, strlen(message)) != (signed int)strlen(message)) {
-                        return -1;
-                }
-                return 0;
-        }
-			
+	if (req->errcode != 0)
+	{
+		snprintf(message, sizeof(message), "HTTP/1.0 %s\r\n", res->status);
+		if (write(socket, message, strlen(message)) != (signed int)strlen(message))
+		{
+			return -1;
+		}
+		return 0;
+	}
 
-        if (req->version == 1.0) {
-                if (res->dirindex == 0 && strcmp(res->status, "200 OK\r\n") == 0) {
-                        char curtime[MAX_TIME];
-                        char mtime[MAX_TIME];
-                        struct tm *curtm;
-                        struct tm *mtm;
-                        time_t now;
+	if (req->version == 1.0)
+	{
+		if (res->dirindex == 0 && strcmp(res->status, "200 OK\r\n") == 0)
+		{
+			char curtime[MAX_TIME];
+			char mtime[MAX_TIME];
+			struct tm *curtm;
+			struct tm *mtm;
+			time_t now;
 
-                        now = time(0);
-                        curtm = localtime(&now);
-                        strftime(curtime, sizeof(curtime), "%a, %d %b %Y %T GMT", curtm);
+			now = time(0);
+			curtm = localtime(&now);
+			strftime(curtime, sizeof(curtime), "%a, %d %b %Y %T GMT", curtm);
 
-                        mtm = localtime(&(res->lastmtime));
-                        strftime(mtime, sizeof(curtime), "%a, %d %b %Y %T GMT", mtm);
+			mtm = localtime(&(res->lastmtime));
+			strftime(mtime, sizeof(curtime), "%a, %d %b %Y %T GMT", mtm);
 
-                        snprintf(message, sizeof(message), "HTTP/1.0 %s\r\nDate: %s\r\nServer: SAS(Super Awesome Server)\r\nLast-Modified: %s\r\nContent-Type: %s\r\nContent-Length: %lld\r\n\r\n", res->status, curtime, mtime, res->contenttype, res->contentlength);
+			snprintf(message, sizeof(message), "HTTP/1.0 %s\r\nDate: %s\r\nServer: SAS(Super Awesome Server)\r\nLast-Modified: %s\r\nContent-Type: %s\r\nContent-Length: %lld\r\n\r\n", res->status, curtime, mtime, res->contenttype, res->contentlength);
+		}
+		else
+		{
+			snprintf(message, sizeof(message), "HTTP/1.0 %s\r\n", res->status);
+		}
 
-                } else {
-                        snprintf(message, sizeof(message), "HTTP/1.0 %s\r\n", res->status);
-                }
-				
-                if (write(socket, message, strlen(message)) != (signed int)strlen(message)) {
-                        syslog(LOG_INFO, "Error writing to socket");
-                        return -1;
-                }
-        }
+		if (write(socket, message, strlen(message)) != (signed int)strlen(message))
+		{
+			syslog(LOG_INFO, "Error writing to socket");
+			return -1;
+		}
+	}
 
+	if (res->dirindex == 1 && req->method == GET)
+	{
+		FTSENT *ent;
+		FTS *ftsp;
+		char *dir[2];
+		dir[0] = req->uri;
+		dir[1] = NULL;
 
-        if (res->dirindex == 1 && req->method == GET) {
-                FTSENT *ent;
-                FTS *ftsp;
-                char *dir[2];
-                dir[0] = req->uri;
-                dir[1] = NULL;
+		ftsp = fts_open(dir, FTS_PHYSICAL, compar);
+		errno = 0;
+		while ((ent = fts_read(ftsp)))
+		{
+			if (errno)
+			{
+				syslog(LOG_INFO, "Error reading FTS");
+				(void)fts_close(ftsp);
+				return -1;
+			}
+			if (ent->fts_level == 1 && ent->fts_info != FTS_DP)
+			{
+				char fname[strlen(ent->fts_name) + 2];
+				(void)snprintf(fname, sizeof(fname), "%s\n", ent->fts_name);
 
-                ftsp = fts_open(dir, FTS_PHYSICAL, compar);
-                errno = 0;
-                while ((ent = fts_read(ftsp))) {
-                        if (errno) {
-                                syslog(LOG_INFO, "Error reading FTS");
-                                (void)fts_close(ftsp);
-                                return -1;
-                        }
-                        if (ent->fts_level == 1 && ent->fts_info != FTS_DP) {
-                                char fname[strlen(ent->fts_name)+2];
-                                (void)snprintf(fname, sizeof(fname), "%s\n", ent->fts_name);
+				if (write(socket, fname, strlen(fname)) != (int)strlen(fname))
+				{
+					syslog(LOG_INFO, "Error writing to socket on FTS");
+					(void)fts_close(ftsp);
+					return -1;
+				}
+			}
+		}
+		(void)fts_close(ftsp);
+		return 0;
+	}
 
-                                if (write(socket, fname, strlen(fname)) != (int) strlen(fname)) {
-                                        syslog(LOG_INFO, "Error writing to socket on FTS");
-                                        (void)fts_close(ftsp);
-                                        return -1;
-                                }
-                        }
-                }
-                (void)fts_close(ftsp);
-                return 0;
-        }
+	if (req->method == GET)
+	{
+		if ((fd = open(req->uri, O_RDONLY)) == -1)
+		{
+			syslog(LOG_INFO, "Error opening GET URI");
+			return -1;
+		}
 
-        if (req->method == GET) {
-                if ((fd = open(req->uri, O_RDONLY)) == -1) {
-                        syslog(LOG_INFO, "Error opening GET URI");
-                        return -1;
-                }
+		while ((readsize = read(fd, buf, sizeof(buf))) > 0)
+		{
+			if ((writesize = write(socket, buf, readsize)) != readsize)
+			{
+				syslog(LOG_INFO, "Error writing to socket on GET");
+				(void)close(fd);
+				return -1;
+			}
+		}
 
-                while ((readsize = read(fd, buf, sizeof(buf))) > 0) {
-                        if ((writesize = write(socket, buf, readsize)) != readsize) {
-                                syslog(LOG_INFO, "Error writing to socket on GET");
-                                (void)close(fd);
-                                return -1;
-                        }
-                }
+		(void)close(fd);
+	}
 
-                (void)close(fd);
-        }
-
-        return 0;
+	return 0;
 }
 
 /*
  * Executes any CGI requests.
  */
-int
-runcgi(int socket, char *uri, char *dir) {
-        char *path, fullpath[MAXPATHLEN];
-        pid_t pid;
+int 
+runcgi(int socket, char *uri, char *dir)
+{
+	char *path, fullpath[MAXPATHLEN];
+	pid_t pid;
 
-        path = strsep(&uri, "?");
-        snprintf(fullpath, MAXPATHLEN, "%s/%s", dir, path);
+	path = strsep(&uri, "?");
+	snprintf(fullpath, MAXPATHLEN, "%s/%s", dir, path);
 
-        if ((pid = fork()) < 0) {
-                syslog(LOG_INFO, "Error forking");
-                return -1;
-        } else if (pid == 0) {
+	if ((pid = fork()) < 0)
+	{
+		syslog(LOG_INFO, "Error forking");
+		return -1;
+	}
+	else if (pid == 0)
+	{
 
-                if (uri != NULL && strlen(uri) > 0) {
-                        char *var;
-                        while ((var = strsep(&uri, "&")) != NULL) {
-                                if (putenv(var) == -1) {
-                                        syslog(LOG_INFO, "Error modifying environment");
-                                        return -1;
-                                }
-                        }
-                }
+		if (uri != NULL && strlen(uri) > 0)
+		{
+			char *var;
+			while ((var = strsep(&uri, "&")) != NULL)
+			{
+				if (putenv(var) == -1)
+				{
+					syslog(LOG_INFO, "Error modifying environment");
+					return -1;
+				}
+			}
+		}
 
-                if (dup2(socket, STDOUT_FILENO) < 0) {
-                        syslog(LOG_INFO, "Error duping STDOUT_FILENO");
-                        exit(EXIT_FAILURE);
-                }
+		if (dup2(socket, STDOUT_FILENO) < 0)
+		{
+			syslog(LOG_INFO, "Error duping STDOUT_FILENO");
+			exit(EXIT_FAILURE);
+		}
 
-                if (dup2(socket, STDERR_FILENO) < 0) {
-                        syslog(LOG_INFO, "Error duping STDERR_FILENO");
-                        exit(EXIT_FAILURE);
-                }
+		if (dup2(socket, STDERR_FILENO) < 0)
+		{
+			syslog(LOG_INFO, "Error duping STDERR_FILENO");
+			exit(EXIT_FAILURE);
+		}
 
-                char command[MAXPATHLEN];
-                snprintf(command, MAXPATHLEN, "%s", fullpath);
-                // execl("/bin/sh", "sh", "-c", command, (char*) 0);
-                execl(command, "", (char*) 0);
+		char command[MAXPATHLEN];
+		snprintf(command, MAXPATHLEN, "%s", fullpath);
+		// execl("/bin/sh", "sh", "-c", command, (char*) 0);
+		execl(command, "", (char *)0);
 
-                return -1;
-        }
+		return -1;
+	}
 
-        (void)wait(NULL);
-        return 0;
+	(void)wait(NULL);
+	return 0;
 }
 
 /*
  * Handles directory pathing based on the presence of a user directory.
  */
-int
-userdirhandler(char *uri, char newuri[]) {
-        char *before, *uname;
-        struct passwd *psw;
+int 
+userdirhandler(char *uri, char newuri[])
+{
+	char *before, *uname;
+	struct passwd *psw;
 
-        before = strsep(&uri, "~");
-        uname = strsep(&uri, "/");
+	before = strsep(&uri, "~");
+	uname = strsep(&uri, "/");
 
-        if ((psw = getpwnam(uname)) == NULL) {
-                syslog(LOG_INFO, "Error on getpwnam()");
-                return -1;
-        }
+	if ((psw = getpwnam(uname)) == NULL)
+	{
+		syslog(LOG_INFO, "Error on getpwnam()");
+		return -1;
+	}
 
-        if (before != NULL && before[0] != '\0') {
-                if (uri != NULL) {
-                        snprintf(newuri, MAXPATHLEN, "%s/%s/sws/%s", before, psw->pw_dir, uri);
-                } else {
-                        snprintf(newuri, MAXPATHLEN, "%s/%s/sws", before, psw->pw_dir);
-                }
-        } else {
-                if (uri != NULL) {
-                        snprintf(newuri, MAXPATHLEN, "%s/sws/%s", psw->pw_dir, uri);
-                } else {
-                        snprintf(newuri, MAXPATHLEN, "%s/sws", psw->pw_dir);
-                }
-        }
+	if (before != NULL && before[0] != '\0')
+	{
+		if (uri != NULL)
+		{
+			snprintf(newuri, MAXPATHLEN, "%s/%s/sws/%s", before, psw->pw_dir, uri);
+		}
+		else
+		{
+			snprintf(newuri, MAXPATHLEN, "%s/%s/sws", before, psw->pw_dir);
+		}
+	}
+	else
+	{
+		if (uri != NULL)
+		{
+			snprintf(newuri, MAXPATHLEN, "%s/sws/%s", psw->pw_dir, uri);
+		}
+		else
+		{
+			snprintf(newuri, MAXPATHLEN, "%s/sws", psw->pw_dir);
+		}
+	}
 
-        return 0;
+	return 0;
 }
-
--- sws.c
+++ sws.c
@@ -12,7 +12,7 @@
 
 #include "sws.h"
 
-int c_opt, d_opt, h_opt, i_opt, l_opt, p_opt, logFd = STDOUT_FILENO, port = 8080, ipv = 6, rootfd;      
+int c_opt, d_opt, h_opt, i_opt, l_opt, p_opt, logFd = STDOUT_FILENO, port = 8080, ipv = 6, rootfd;
 char *dir, *cgiDir, *addr, *file, *ipAddr;
 
 int main(int, char **);
@@ -20,125 +20,141 @@ int main(int, char **);
 /*
  * Determines if a directory pathing is valid/
  */
-int
+int 
 testDir(char *dir)
 {
-        DIR *dirTest;
-        dirTest = opendir(dir);
-        if (dirTest) {
-                closedir(dirTest);
-                return EXIT_SUCCESS;
-        } else {
-                fprintf(stderr, "invalid directory '%s'\n", dir);
-                return EXIT_FAILURE;
-        }
+	DIR *dirTest;
+	dirTest = opendir(dir);
+	if (dirTest)
+	{
+		closedir(dirTest);
+		return EXIT_SUCCESS;
+	}
+	else
+	{
+		fprintf(stderr, "invalid directory '%s'\n", dir);
+		return EXIT_FAILURE;
+	}
 }
 
 /*
  * Parses and validations options. Starts the server depending on the input instructions.
  */
-int
+int 
 main(int argc, char **argv)
 {
-        char *temp = NULL;
+	char *temp = NULL;
 	char opt;
 
-        while (optind < argc) {
-                if ((opt = getopt(argc, argv, "c:dhi:l:p:")) != -1) {
-                        switch (opt) {
-                                case 'c':
-                                        cgiDir = optarg;
-                                        c_opt = 1;
-                                        break;
+	while ((opt = getopt(argc, argv, "c:dhi:l:p:")) != -1)
+	{
+		switch (opt)
+		{
+		case 'c':
+			cgiDir = optarg;
+			c_opt = 1;
+			break;
 
-                                case 'd':
-                                        d_opt = 1;
-                                        break;
+		case 'd':
+			d_opt = 1;
+			break;
 
-                                case 'h':
-                                        h_opt = 1;
-                                        printf("Usage: %s [-c dir] [-dh] [-i address] [-l file] [-p port] dir\n", argv[0]);
-                                        exit(EXIT_SUCCESS);
+		case 'h':
+			h_opt = 1;
+			printf("Usage: %s [-c dir] [-dh] [-i address] [-l file] [-p port] dir\n", argv[0]);
+			exit(EXIT_SUCCESS);
 
-                                case 'i':
-                                        ipv = verifyIp(optarg);
-                                        if (ipv == -1) {
-                                                fprintf(stderr, "%s: invalid IP Address '%s'\n", argv[0], optarg);
-                                                exit(EXIT_FAILURE);
-                                        }
-                                        i_opt = 1;
-                                        break;
-
-                                case 'l':
-                                        file = optarg;
-                                        if((logFd = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRWXU)) == -1) {
-                                                if ((logFd = openat(logFd, basename(file), O_WRONLY  | O_CREAT | O_APPEND, S_IRWXU)) < 0) {
-                                                        fprintf(stderr, "%s: invalid logging file '%s': %s\n", argv[0], optarg, strerror(errno));
-                                                        exit(EXIT_FAILURE);
-                                                }
-                                                fprintf(stderr, "%s: invalid logging file '%s': %s\n", argv[0], optarg, strerror(errno));
-                                                exit(EXIT_FAILURE);
-                                        }
-					l_opt = 1;
-					break;
-
-                                case 'p':
-                                        port = (int)strtol(optarg, &temp, 10);
-                                        errno = 0;
-					if (errno != 0) {
-                                                fprintf(stderr, "%s: invalid port '%s'\n", argv[0], optarg);
-                                                exit(EXIT_FAILURE);
-                                        }
-                                        p_opt = 1;
-                                        break;
-
-                                default:
-                                        fprintf(stderr, "%s: invalid option '%c'\n", argv[0], opt);
-                                        exit(EXIT_FAILURE);
-                        }
-                } else {
-                	dir = argv[optind];
-
-			if (dir == NULL) {	/* Assume this current directory */
-				dir = ".";
+		case 'i':
+			ipv = verifyIp(optarg);
+			if (ipv == -1)
+			{
+				fprintf(stderr, "%s: invalid IP Address '%s'\n", argv[0], optarg);
+				exit(EXIT_FAILURE);
 			}
-			optind++;
-       		}
+			i_opt = 1;
+			break;
+
+		case 'l':
+			file = optarg;
+			if ((logFd = open(file, O_WRONLY | O_CREAT | O_APPEND, S_IRWXU)) == -1)
+			{
+				if ((logFd = openat(logFd, basename(file), O_WRONLY | O_CREAT | O_APPEND, S_IRWXU)) < 0)
+				{
+					fprintf(stderr, "%s: invalid logging file '%s': %s\n", argv[0], optarg, strerror(errno));
+					exit(EXIT_FAILURE);
+				}
+				fprintf(stderr, "%s: invalid logging file '%s': %s\n", argv[0], optarg, strerror(errno));
+				exit(EXIT_FAILURE);
+			}
+			l_opt = 1;
+			break;
+
+		case 'p':
+			errno = 0;
+			port = atoi(optarg);
+			if (errno != 0)
+			{
+				fprintf(stderr, "%s: invalid port '%s'\n", argv[0], optarg);
+				exit(EXIT_FAILURE);
+			}
+
+			if (port < 1024 || port > 49151) {
+				fprintf(stderr, "Invalid port number, can only between 1024~49151.\n");
+			}
+			
+			p_opt = 1;
+			break;
+
+		default:
+			fprintf(stderr, "%s: invalid option '%c'\n", argv[0], opt);
+			exit(EXIT_FAILURE);
+		}
 	}
 
-	if ((d_opt == 0) && (l_opt == 0)) {	/* No logging, redirect logFd to /dev/null */
-		if ((logFd = open("/dev/null", O_WRONLY)) == -1) {
+	dir = argv[optind];
+
+	if (dir == NULL)
+	{ /* Assume this current directory */
+		dir = ".";
+	}
+
+	if ((d_opt == 0) && (l_opt == 0))
+	{ /* No logging, redirect logFd to /dev/null */
+		if ((logFd = open("/dev/null", O_WRONLY)) == -1)
+		{
 			perror("redirect log to /dev/null");
 			exit(EXIT_FAILURE);
 		}
 	}
 
-        if (testDir(dir) != EXIT_SUCCESS) { /* Checking dir right before the networking code starts (moved from in opt loop) */
-                exit(EXIT_FAILURE);
-        }
+	if (testDir(dir) != EXIT_SUCCESS)
+	{ /* Checking dir right before the networking code starts (moved from in opt loop) */
+		exit(EXIT_FAILURE);
+	}
 
-        if (chdir(dir) != 0) {
-                perror("chdir");
-                exit(EXIT_FAILURE);
-        }
+	if (chdir(dir) != 0)
+	{
+		perror("chdir");
+		exit(EXIT_FAILURE);
+	}
 
 	//	if (chroot(dir) != 0) {
 	//		perror("chroot");
 	//		exit(EXIT_FAILURE);
 	//	}
 
-        openlog(argv[0], LOG_PID, 0);       /* Opens system logging to track server errors */
+	openlog(argv[0], LOG_PID, 0); /* Opens system logging to track server errors */
 
-        if (d_opt == 0) {
-                if (daemon(0, 1) == -1) {
-                        perror("daemon");
-                        exit(EXIT_FAILURE);
-                }
-        }
+	if (d_opt == 0)
+	{
+		if (daemon(0, 1) == -1)
+		{
+			perror("daemon");
+			exit(EXIT_FAILURE);
+		}
+	}
 
-        startServer();
+	startServer();
 
-        return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
-
-
