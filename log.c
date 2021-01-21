49ccea0ac1954232026e9d97766e2c8331ac3109
Michael <michael827xmy@hotmail.com>
2021-01-21 13:45:52 -0500

    error and format fix

--- log.c
+++ log.c
@@ -17,29 +17,31 @@
  * Logs requests made to the server to the designated logging file. This file is specified as a
  * parameter of the 'l' option on server startup and is validated/opened in sws.c
  */
-int
-writeLog(const char *rip, struct tm *time, char *firstLine, const char *status, long long contentLength)
+int writeLog(const char *rip, struct tm *time, char *firstLine, const char *status, long long contentLength)
 {
-        char timeBuf[TIME_STR_MAX];
-        (void)strftime(timeBuf, TIME_STR_MAX, "%Y-%m-%dT%H:%M:%SZ", time);
+	char timeBuf[TIME_STR_MAX];
+	(void)strftime(timeBuf, TIME_STR_MAX, "%Y-%m-%dT%H:%M:%SZ", time);
 
-        char *contentBuf = malloc(strlen(rip)+sizeof(timeBuf)+strlen(firstLine)+strlen(status)+sizeof(contentLength)+1);
-        if (contentBuf == NULL) {
-                syslog(LOG_INFO, "Error allocating memory for content length - string cast");
-                return -1;
-        }
+	char *contentBuf = malloc(strlen(rip) + strlen(timeBuf) + strlen(firstLine) + strlen(status) + sizeof(contentLength) + 1);
+	if (contentBuf == NULL)
+	{
+		syslog(LOG_INFO, "Error allocating memory for content length - string cast");
+		return -1;
+	}
 
-        if (snprintf(contentBuf, sizeof(contentBuf), "%s %s %s %s %lld", rip, timeBuf, firstLine, status, contentLength) < 0) { /* Gets rid of newline char */
-                syslog(LOG_INFO, "Failed at snprintf()");
-                return -1;
-        }
+	if (snprintf(contentBuf, sizeof(contentBuf), "%s %s %s %s %lld", rip, timeBuf, firstLine, status, contentLength) < 0)
+	{ /* Gets rid of newline char */
+		syslog(LOG_INFO, "Failed at snprintf()");
+		return -1;
+	}
 
-        if (write(logFd, contentBuf, sizeof(contentBuf)) < 0) {
-                syslog(LOG_INFO, "Error writing IP to log");
-                return -1;
-        }
+	if (write(logFd, contentBuf, sizeof(contentBuf)) < 0)
+	{
+		syslog(LOG_INFO, "Error writing IP to log");
+		return -1;
+	}
 
-        (void)free(contentBuf);
+	(void)free(contentBuf);
 
-        return 0;
+	return 0;
 }
