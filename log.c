ca425058feefb9c0e6c46945f8dd37d66a21f9e6
Michael <michael827xmy@hotmail.com>
2021-01-21 16:47:44 -0500

    log message format

--- log.c
+++ log.c
@@ -21,13 +21,14 @@
  */
 int writeLog(const char *rip, struct tm *time, char *firstLine, const char *status, long long contentLength)
 {
-	char timeBuf[TIME_STR_MAX], statusCode[CODELEN];
+	char timeBuf[TIME_STR_MAX], statusCode[CODELEN], contentLen[BUFSIZ];
 
 	(void)strlcpy(statusCode, status, CODELEN);
 
 	(void)strftime(timeBuf, TIME_STR_MAX, "%Y-%m-%dT%H:%M:%SZ", time);
+	(void)snprintf(contentLen, BUFSIZ, "%lld", contentLength);
 
-	int len = strlen(rip) + sizeof(timeBuf) + strlen(firstLine) + strlen(statusCode) + sizeof(contentLength) + 1;
+	int len = strlen(rip) + sizeof(timeBuf) + strlen(firstLine) + strlen(statusCode) + strlen(contentLen) + 1;
 	char *contentBuf = malloc(len);
 	
 	if (contentBuf == NULL)
@@ -36,7 +37,7 @@ int writeLog(const char *rip, struct tm *time, char *firstLine, const char *stat
 		return -1;
 	}
 
-	if (snprintf(contentBuf, len, "%s %s \"%s\" %s %lld\n", rip, timeBuf, firstLine, statusCode, contentLength) < 0)
+	if (snprintf(contentBuf, len, "%s %s \"%s\" %s %s\n", rip, timeBuf, firstLine, statusCode, contentLen) < 0)
 	{ /* Gets rid of newline char */
 		syslog(LOG_INFO, "Failed at snprintf()");
 		return -1;
